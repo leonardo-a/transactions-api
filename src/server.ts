bceb0a72f4b8a2616cabcb307602a2cf739700fa
Leonardo-a <leolealantao@gmail.com>
2023-04-07 12:37:45 -0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/hello', async (request, reply) => {
+  return 'Hello World!'
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('Server Listening to 3333')
+  })
